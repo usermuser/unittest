d09e278fb292c7be479781edfae35b6454d0492a
usermuser <lwrdrstv@gmail.com>
2020-02-10 00:00:33 +0500

    chapter3 - added TestLoader

--- chapter3/test_runner.py
+++ chapter3/test_runner.py
@@ -1,10 +1,17 @@
 import unittest
 import calc_tests
 
-calcTestSuite = unittest.TestSuite()
-calcTestSuite.addTest(unittest.makeSuite(calc_tests.CalcBasicTests))
-calcTestSuite.addTest(unittest.makeSuite(calc_tests.CalcExTests))
-print(f"Count of tests: {calcTestSuite.countTestCases()} \n")
+testCases = []
+testCases.append(calc_tests.CalcBasicTests)
+testCases.append(calc_tests.CalcExTests)
+
+testLoad = unittest.TestLoader()
+
+suites = []
+for tc in testCases:
+    suites.append(testLoad.loadTestsFromTestCase(tc))
+
+res_suite = unittest.TestSuite(suites)
 
 runner = unittest.TextTestRunner(verbosity=2)
-runner.run(calcTestSuite)
+runner.run(res_suite)
